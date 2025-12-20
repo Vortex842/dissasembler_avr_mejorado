8e028b80c9107d698e5bdf9cf95850c704eb1b6d
Vortex842 <nahuelat.8@gmail.com>
2025-12-20 17:51:36 -0300

    Inciando

---
+++ test/dissasembler_avr_mejorado_test.dart
@@ -0,0 +1,8 @@
+import 'package:dissasembler_avr_mejorado/dissasembler_avr_mejorado.dart';
+import 'package:test/test.dart';
+
+void main() {
+  test('calculate', () {
+    expect(calculate(), 42);
+  });
+}
