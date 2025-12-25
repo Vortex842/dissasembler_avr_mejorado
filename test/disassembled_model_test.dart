8e0d94435cc8af8262b2f51948cd51d119c82ecf
Vortex842 <nahuelat.8@gmail.com>
2025-12-25 20:55:51 -0300

    Haciendo TDD a DisassembledModel

---
+++ test/disassembled_model_test.dart
@@ -0,0 +1,45 @@
+import 'package:test/test.dart';
+
+import '../bin/models/disassembled_model.dart';
+
+void main() {
+  group(
+    'DisassembledModel Tests',
+    () {
+      final shortDecodedInstr = DisassembledModel(
+        text: "LDI R16, 0xFF",
+        countWords: 1,
+        rawWord: 0xEFFF,
+      );
+
+      final longDecodedInstr = DisassembledModel(
+        text: "LDS R16, 0x0150",
+        countWords: 2,
+        rawWord: 0x90000150,
+      );
+
+      test(
+        'Debe almacenar correctamente los datos de una instrucción decodificada',
+        () {
+          expect(shortDecodedInstr.text, equals("LDI R16, 0xFF"),
+              reason: 'El texto debe guardarse intacto');
+
+          expect(shortDecodedInstr.countWords, equals(1),
+              reason: 'La longitud debe coincidir');
+
+          expect(shortDecodedInstr.rawWord, equals(0xEFFF),
+              reason: 'El código hex original debe preservarse');
+        },
+      );
+
+      test(
+        'El metodo "toString" debe devolver el texto de la instrucción (para impresión limpia)',
+        () {
+          expect(longDecodedInstr.toString(), equals("LDS R16, 0x0150"),
+              reason:
+                  'El toString debe ser limpio para facilitar la impresión en consola en el main.');
+        },
+      );
+    },
+  );
+}
